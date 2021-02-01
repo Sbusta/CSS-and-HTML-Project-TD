5ad121db30e61d624e28d95e7d846818ad04d00d
Sebastian Bustamante <sebastianbustamante@MacBooks-MacBook-Pro.local>
2021-02-01 16:16:43 -0500

    first commit of the css project

---
+++ JS/main.js
@@ -0,0 +1,49 @@
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
+
+toggleButton.addEventListener('click',function(){
+    if(mobileNav.style.display == 'flex'){
+        mobileNav.style.display = 'none';
+    }else{mobileNav.style.display = 'flex';}
+})
+
+  $(".nav__button").on("click", function (e) {
+    e.preventDefault();
+    const href = $(this).attr("href");
+    $("html, body").animate({ scrollTop: $(href).offset().top }, 600);
+});
+
+$(".mobile-nav__button").on("click", function (e) {
+    e.preventDefault();
+    const href = $(this).attr("href");
+    $("html, body").animate({ scrollTop: $(href).offset().top }, 600);
+});
+
+$(".button__item_learn-more").on("click", function (e) {
+    e.preventDefault();
+    const href = $(this).attr("href");
+    $("html, body").animate({ scrollTop: $(href).offset().top }, 600);
+});
+
+$(".button__item_contact-us").on("click", function (e) {
+    e.preventDefault();
+    const href = $(this).attr("href");
+    $("html, body").animate({ scrollTop: $(href).offset().top }, 600);
+});
+
+
+$(window).scroll(function() {
+    var position = $(this).scrollTop();
+    $('section').each(function() {
+        var target = $(this).offset().top;
+        var id = $(this).attr('id');
+        
+        if (position >= target) {
+          $('.nav__button').removeClass('highlight');
+          $('.mobile-nav__button').removeClass('highlight');
+
+          $('#' + id+'n').addClass('highlight');
+          $('#' + id+'m').addClass('highlight');
+        }
+    });
+  });
